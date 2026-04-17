c2d0a4aa9c1ed7d4e59de491e9eb03e10d41a7eb
sakamsiddu-svg <sakamsiddu@gmail.com>
2026-04-17 21:59:29 +0530

    Update # streamlit_app.py

--- # streamlit_app.py
+++ # streamlit_app.py
@@ -1,28 +1,28 @@
-# streamlit_app.py
-
-# venv\Scripts\activate  
-
 import numpy as np
-# streamlit_app.py
+cd "c:/Users/Krishna Mohan/Desktop/signature detection/signature detection"
+
 import streamlit as st
 from PIL import Image
 from tensorflow.keras.models import load_model
+from utils.preprocess import preprocess_image  # Import this
 
 # Load model
-model = load_model("model/signature_model.h5")
+from tensorflow.keras.models import load_model
+
+model = load_model("model/signature_model.h5", compile=False)
 
 st.title("Signature Verification")
 st.write("Upload a signature image to verify if it's genuine or forged.")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
+streamlit run streamlit_app.py
 def preprocess_image(img):
     img = img.resize((128, 128))
     img = np.array(img) / 255.0
     img = np.expand_dims(img, axis=0)
     return img
 
-if uploaded_file is not None:
+if uploaded_file:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Signature', use_column_width=True)
     st.write("Processing...")
